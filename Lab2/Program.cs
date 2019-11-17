b0a34936cd97ca103b5dc11c87a1216bd848012e
alekhin sergey <alekhin1991@gmail.com>
2019-11-18 01:08:21 +0300

    add class diagram

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -12,15 +12,18 @@ namespace Lab2
         {
             Console.Title = "Алехин Сергей ИУ5-31Б";
             double dWidth = 2;
-            double dHeight = 1;
-            double dSide = 3;
-            double dRadius = 4;
+            double dHeight = 3;
+            double dSide = 4;
+            double dRadius = 5;
             Console.WriteLine($"Прямоугольник: ширина = {dWidth}, длина = {dHeight}");
             var rectangle = new Rectangle(dWidth, dHeight);
+            Console.WriteLine($"{rectangle.ToString()}");
             Console.WriteLine($"Квадрат: сторона = {dSide}");
             var square = new Square(dSide);
+            Console.WriteLine($"{square.ToString()}");
             Console.WriteLine($"Круг: радиус = {dRadius}");
             var circle = new Circle(dRadius);
+            Console.WriteLine($"{circle.ToString()}");
 
             Console.Read();
         }
