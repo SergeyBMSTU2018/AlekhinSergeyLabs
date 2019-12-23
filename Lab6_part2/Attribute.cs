6dde7a3ab03acd3e76e0a85bfa0d1e6642dd0862
alekhin sergey <alekhin1991@gmail.com>
2019-12-23 23:08:51 +0300

    add Lab6_part2

---
+++ Lab6_part2/Attribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_part2
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class NewAttribute : Attribute
+    {
+        public string Description { get; set; }
+
+        public NewAttribute() { }
+        public NewAttribute(string y)
+        {
+            Description = y;
+        }
+    }
+}
--- Lab6_part2/Program.cs
+++ Lab6_part2/Program.cs
@@ -3,13 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Lab6_part2
 {
     class Program
     {
+        public static bool GetPropertyAttribute(PropertyInfo checkType, Type attributeType, out object attribute)
+        {
+            bool Result = false;
+            attribute = null;
+
+            //Поиск атрибутов с заданным типом
+            var isAttribute = checkType.GetCustomAttributes(attributeType, false);
+            if (isAttribute.Length > 0)
+            {
+                Result = true;
+                attribute = isAttribute[0];
+            }
+
+            return Result;
+        }
+
         static void Main(string[] args)
         {
+            Type t = typeof(TestClass);
+
+            Console.WriteLine("Тип " + t.FullName + " унаследован от " + t.BaseType.FullName);
+            Console.WriteLine("Пространство имен " + t.Namespace);
+            Console.WriteLine("Находится в сборке " + t.AssemblyQualifiedName);
+
+            Console.WriteLine("\nКонструкторы:");
+            foreach (var x in t.GetConstructors())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nМетоды:");
+            foreach (var x in t.GetMethods())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nСвойства:");
+            foreach (var x in t.GetProperties())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nПоля данных (public):");
+            foreach (var x in t.GetFields())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nСвойства, помеченные атрибутом:");
+            foreach (var x in t.GetProperties())
+            {
+                object attrObj;
+                if (GetPropertyAttribute(x, typeof(NewAttribute), out attrObj))
+                {
+                    NewAttribute attr = attrObj as NewAttribute;
+                    Console.WriteLine(x.Name + " - " + attr.Description);
+                }
+            }
+
+            Console.WriteLine("\nВызов метода:");
+
+            //Создание объекта
+            //ForInspection fi = new ForInspection();
+
+            //Можно создать объект через рефлексию
+            TestClass fi = (TestClass)t.InvokeMember(null, BindingFlags.CreateInstance, null, null, new object[] { });
+
+            //Параметры вызова метода
+            object[] parameters = new object[] { 3, false };
+
+            //Вызов метода
+            object Result = t.InvokeMember("st", BindingFlags.InvokeMethod, null, fi, parameters);
+            Console.WriteLine("st(3,false)={0}", Result);
+
+            Console.ReadKey();
         }
     }
 }
